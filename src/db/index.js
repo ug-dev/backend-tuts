92fb9aed18116e264844b77851452c3b427ef13d
UmangGadhavana <gadhavanaumang007@gmail.com>
2023-12-05 19:10:55 +0530

    database connection setup

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log("🚀🚀🚀 MONGODB CONNETED !!!");
+  } catch (error) {
+    console.log(
+      "🚀 ~ file: index.js:8 ~ connectDB ~ MongoDB Error error:",
+      error
+    );
+    process.exit(1);
+  }
+};
+
+export default connectDB;
