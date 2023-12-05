0878ccc1de1f62938c38f374c4b2ef1b7eab5d7b
UmangGadhavana <gadhavanaumang007@gmail.com>
2023-12-05 19:55:34 +0530

    utis config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv";
+import express from "express";
+
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
+const app = express();
+const port = process.env.PORT || 8000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at port : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO_DB Error !!");
+    console.log("🚀 ~ file: index.js:17 ~ error:", error);
+  });
