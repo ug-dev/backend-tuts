5bd746c3cea108c84c236fcad2e2fbb84a2a25b0
UmangGadhavana <gadhavanaumang007@gmail.com>
2023-12-11 18:46:12 +0530

    register controller bugs fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,16 +5,16 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.route("/register").post(
-  upload.fields[
-    ({
+  upload.fields([
+    {
       name: "avatar",
       maxCount: 1,
     },
     {
       name: "coverImage",
       maxCount: 1,
-    })
-  ],
+    },
+  ]),
   registerUser
 );
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file has been uploaded successfully
-    console.log("File is uploaded in cloudinary !!!", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     /* 
